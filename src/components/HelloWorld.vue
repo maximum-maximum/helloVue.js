79b1ec251395c3d2d989a392ea44522f55c00463
Koki Makishima <mr_ver74@yahoo.co.jp>
2022-06-08 13:57:04 +0900

    テンプレート参照（this.$ref.名前）

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,27 @@
 <template>
-  <div class="alert alert-info">
-    <h2>{{ title }}</h2>
-    <p>{{ message }}</p>
-    <hr />
-    <div>
-      <input class="form-control" type="text" v-model="input" />
-      <button class="btn btn-info mt-2" v-on:click="doAction">Click</button>
-    </div>
+  <div className="alert alert-primary">
+    <h1>{{ title }}</h1>
+    <p ref="msg">{{ message }}</p>
+    <button class="btn btn-primary" v-on:click="doAction">Click</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    title: String,
-  },
   data() {
     return {
-      message: "お名前は？",
-      input: "no name",
+      title: "HelloWorld",
+      message: "This is sample message.",
     };
+  },
+  mounted() {
+    this.counter = 0;
   },
   methods: {
     doAction() {
-      this.message = "こんにちは、" + this.input + "さん！";
-      this.$emit("result-event", this.input);
+      this.counter++;
+      this.$refs.msg.innerHTML += "<h6>counted: " + this.counter + "</h6>";
     },
   },
 };
